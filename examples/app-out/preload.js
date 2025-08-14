c706ad177dfe23e055264649f0c66a1b878ed3c1
ihainan <ihainan72@gmail.com>
2025-08-14 21:10:43 +0800

    chore: update gitignore to exclude test generated files and directories
    - Add examples/app-out/ directory to gitignore
    - Exclude zip files generated during testing
    - Ignore Python test files and cache directories
    - Remove test artifacts from git tracking

--- examples/app-out/preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  onFileOpened: (callback) => ipcRenderer.on('file-opened', (_event, value) => callback(value))
-});
